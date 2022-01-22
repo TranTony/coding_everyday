d421a258692997fdb0b083d204092ac75f60a8a1
TranTony <tranthanhtrung1990@gmail.com>
2022-01-22 10:00:24 +0700

    Update find_third_max.py

--- find_third_max.py
+++ find_third_max.py
@@ -36,3 +36,13 @@ class Solution:
         else:
             srt_nums = sorted(nums, reverse=True)
             return srt_nums[2]
+# save more memory
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        if len(nums) < 3:
+            return max(nums)
+        else:
+            nums.remove(max(nums))
+            nums.remove(max(nums))
+        return max(nums)
